b6df1cbd48ef7caa12205af8e6e87be37e3d99fc
Sal <osmansalih717@gmail.com>
2023-02-18 11:04:34 -0500

    First Commit

---
+++ src/Package1/Test1.java
@@ -0,0 +1,14 @@
+package Package1;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		String hungry = "yes";
+		if(hungry == "yes")
+			System.out.println("Please eat something before joing the class....");
+		else
+			System.out.println("Let's begin our class.....");
+
+	}
+
+}
